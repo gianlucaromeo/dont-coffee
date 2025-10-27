49cab4fa7c1b39727e4a1f50144b2d4fb56b6fba
gianluca-romeo <gianlucaromeo@outlook.com>
2025-10-27 16:08:56 +0200

    chore: Add custom `prefetch` and `HydrateClient

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrateClient, prefetch, trpc } from "@/trpc/server";
 import { ClientGreeting } from "./client-greeting";
 
 export default async function Home() {
-  const queryClient = getQueryClient();
-
-  queryClient.prefetchQuery(
-    trpc.hello.queryOptions({
-      text: "Gianluca",
-    })
-  );
+  prefetch(trpc.hello.queryOptions({ text: "Gianluca" }));
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <HydrateClient>
       <ClientGreeting />
-    </HydrationBoundary>
+    </HydrateClient>
   );
 }
